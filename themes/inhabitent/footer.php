c7485f3a58106079b4b00b935fdec241608647c9
Andre Moura <andremarquesmoura@gmail.com>
2019-02-15 13:46:02 -0800

    Initial commit

---
+++ themes/inhabitent/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					<p>Contact Info</p>
+					<a href="mailto:info@inhabitent.com">info@inhabitent.com<a>
+					<a href="tel:7784567891">778-456-7891</a>
+					<p>Business Hours</p>
+					<p><span class="bold">Monday-Friday:</span> 9am to 5pm</p>
+					<p><span class="bold">Saturday:</span> 10am to 2pm</p>
+					<p><span class="bold">Sunday:</span> Closed</p>
+					<!-- <a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a> -->
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
