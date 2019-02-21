62b0dc9f0b6c30d592dd9cb0fc2b1d7210eadff9
Andre Moura <andremarquesmoura@gmail.com>
2019-02-21 15:07:25 -0800

    Style the header bar

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -7,34 +7,34 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-		
-		<?php 
-			$latest_posts = inhabitent_get_latest_posts();
-			foreach ( $latest_posts as $post ) : setup_postdata( $post );
-		?>
-		<div>
-			<?php
-				if(has_post_thumbnail()) {
-					the_post_thumbnail('medium_large');
-				}
-			?>
-			<?php 
-				the_date();
-				echo comments_number();
-				the_title();
-			?>
-				<a href="<?php the_permalink(); ?>">Read entry</a>
-			<?php endforeach; 
 
-			wp_reset_postdata();
-			?>
-			
-		</div>
+        <?php 
+        $latest_posts = inhabitent_get_latest_posts();
+        foreach ($latest_posts as $post): setup_postdata($post);
+            ?>
+        <div>
+            <?php
+            if (has_post_thumbnail()) {
+                the_post_thumbnail('medium_large');
+            }
+            ?>
+            <?php 
+            the_date();
+            echo comments_number();
+            the_title();
+            ?>
+            <a href="<?php the_permalink(); ?>">Read entry</a>
+            <?php endforeach;
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        wp_reset_postdata();
+        ?>
 
-<?php get_footer(); ?>
+        </div>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?> 
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -5,36 +5,43 @@
  * @package Inhabitent_Theme
  */
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<?php wp_enqueue_style( 'prefix-font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.1/css/font-awesome.min.css', array(), '4.6.1' ); ?>
 
-	<?php wp_head(); ?>
-	</head>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <?php wp_enqueue_style('prefix-font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.1/css/font-awesome.min.css', array(), '4.6.1'); ?>
 
-	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+    <?php wp_head(); ?>
+</head>
 
-			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<!-- <h1 class="site-title screen-reader-text"></h1> -->
-					<!-- <p class="site-description"><?php bloginfo( 'description' ); ?></p> -->
-					<div class="logo">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<h1 class="screen-reader-text">Inhabitent</h1></a>
-					</div>
-				</div><!-- .site-branding -->
+<body <?php body_class(); ?>>
+    <div id="page" class="hfeed site">
+        <a class="skip-link screen-reader-text" href="#content">
+            <?php echo esc_html('Skip to content'); ?></a>
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
+        <header id="masthead" class="site-header" role="banner">
+            <div class="container">
+                <div class="site-branding">
+                    <!-- <h1 class="site-title screen-reader-text"></h1> -->
+                    <!-- <p class="site-description"><?php bloginfo('description'); ?></p> -->
+                    <div class="logo">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                            <h1 class="site-logo"><span class="screen-reader-text">Inhabitent</span></h1>
+                        </a>
+                    </div>
+                </div><!-- .site-branding -->
 
-			<div id="content" class="site-content">
+                <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                        <?php echo esc_html('Primary Menu'); ?></button>
+                    <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+                </nav><!-- #site-navigation -->
+            </div>
+        </header><!-- #masthead -->
+
+        <div id="content" class="site-content"> 
