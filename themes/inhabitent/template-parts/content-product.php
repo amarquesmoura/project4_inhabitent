f599487392b0f53af796fe679f701732690a7efa
Andre Moura <andremarquesmoura@gmail.com>
2019-02-24 13:08:34 -0800

    Style single product pages

--- themes/inhabitent/template-parts/content-product.php
+++ themes/inhabitent/template-parts/content-product.php
@@ -8,28 +8,43 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+    <div class="product-image">
+        <?php if (has_post_thumbnail()): ?>
+        <?php the_post_thumbnail('large'); ?>
+        <?php endif; ?>
+    </div>
+    <div class="product-text">
+        <header class="entry-header-product">
+            <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header><!-- .entry-header -->
 
-	</header><!-- .entry-header -->
+        <div class="entry-content">
+            <p class="price">
+                <?php echo CFS()->get('product_price'); ?>
+            </p>
+            <?php the_content(); ?>
+            <?php
+            wp_link_pages(array(
+                'before' => '<div class="page-links">' . esc_html('Pages:'),
+                'after'  => '</div>',
+            ));
+            ?>
+            <div class="social-media">
+                <button type="button" class="black-btn">
+                    <i class="fab fa-facebook"></i>Like
+                </button>
+                <button type="button" class="black-btn">
+                    <i class="fab fa-twitter"></i>Tweet
+                </button>
+                <button type="button" class="black-btn">
+                    <i class="fab fa-pinterest"></i>Pin
+                </button>
+            </div>
+        </div><!-- .entry-content -->
+    </div>
 
-	<span><?php echo CFS()->get( 'product_price' ); ?></span>
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php inhabitent_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+    <footer class="entry-footer">
+        <?php inhabitent_entry_footer(); ?>
+    </footer><!-- .entry-footer -->
+</article><!-- #post-## --> 
