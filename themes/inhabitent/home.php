34680327fce2d7a90c79ceac8b43c8b33655256c
Andre Moura <andremarquesmoura@gmail.com>
2019-02-24 00:55:10 -0800

    Style About and Find Us pages

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -7,17 +7,19 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+        <?php while (have_posts()): the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+        <?php get_template_part('template-parts/content'); ?>
 
-			<?php endwhile; // End of the loop. ?>
+        <p><a href="<?php echo get_permalink() ?>" class="more black-btn">Read more &#8594;</a>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            <?php endwhile; ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_footer(); ?> 
