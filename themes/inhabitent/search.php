728074926e15642e29537795fe07ed02446c8edf
Andre Moura <andremarquesmoura@gmail.com>
2019-02-24 17:12:41 -0800

    Style search page

--- themes/inhabitent/search.php
+++ themes/inhabitent/search.php
@@ -7,32 +7,35 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+        <?php if (have_posts()): ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+        <header class="page-header">
+            <h1 class="page-title">
+                <?php printf(esc_html('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?>
+            </h1>
+        </header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+        <?php  /* Start the Loop */ ?>
+        <?php while (have_posts()): the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+        <?php get_template_part('template-parts/content', 'search'); ?>
 
-			<?php endwhile; ?>
 
-			<?php inhabitent_numbered_pagination(); ?>
+        <?php endwhile; ?>
 
-		<?php else : ?>
+        <?php inhabitent_numbered_pagination(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+        <?php else: ?>
 
-		<?php endif; ?>
+        <?php get_template_part('template-parts/content', 'none'); ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+        <?php endif; ?>
+
+    </main><!-- #main -->
+</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_footer(); ?> 
