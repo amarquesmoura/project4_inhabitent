24966de8d65011d04543d9d28aa7716319363bf6
Andre Moura <andremarquesmoura@gmail.com>
2019-02-24 20:00:32 -0800

    Style product listing pages

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -11,42 +11,40 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
 
         <?php if (have_posts()): ?>
+        <div class="container">
+            <header class="page-header">
+                <h1 class="page-title">Shop Stuff</h1>
 
-        <header class="page-header">
-            <h1 class="page-title">Shop Stuff</h1>
-            <hr>
-            <ul class="product-categories-menu">
-                <?php
-                $product_terms = get_terms('product_type');
-                foreach ($product_terms as $product_term): ?>
-                <li>
-                    <p><a href="<?php echo get_term_link($product_term); ?>" class="btn">
-                            <?php echo $product_term->slug; ?></a>
-                    </p>
-                </li>
-                <?php endforeach ?>
+                <ul class="product-categories-menu">
+                    <?php
+                    $product_terms = get_terms('product_type');
+                    foreach ($product_terms as $product_term): ?>
+                    <li>
+                        <p><a href="<?php echo get_term_link($product_term); ?>">
+                                <?php echo $product_term->slug; ?></a>
+                        </p>
+                    </li>
+                    <?php endforeach ?>
 
-        </header><!-- .page-header -->
+            </header><!-- .page-header -->
+            <div class="product-grid">
+                <?php  /* Start the Loop */ ?>
+                <?php while (have_posts()): the_post(); ?>
 
-        <?php  /* Start the Loop */ ?>
-        <?php while (have_posts()): the_post(); ?>
+                <?php get_template_part('template-parts/content'); ?>
 
-        <?php
-        get_template_part('template-parts/content');
-        ?>
+                <?php endwhile; ?>
 
-        <?php endwhile; ?>
+                <?php the_posts_navigation(); ?>
 
-        <?php the_posts_navigation(); ?>
+                <?php else: ?>
 
-        <?php else: ?>
+                <?php get_template_part('template-parts/content', 'none'); ?>
 
-        <?php get_template_part('template-parts/content', 'none'); ?>
-
-        <?php endif; ?>
-
+                <?php endif; ?>
+            </div>
+        </div>
     </main><!-- #main -->
 </div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?> 
--- themes/inhabitent/template-parts/content.php
+++ themes/inhabitent/template-parts/content.php
@@ -6,25 +6,21 @@
  */
 
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php if (has_post_thumbnail()): ?>
-        <?php the_post_thumbnail('full'); ?>
+<div class="product-grid-card">
+    <?php if (has_post_thumbnail()): ?>
+    <div class="product-thumbnail">
+        <a href="<?php esc_url(get_permalink()) ?>" rel="bookmark">
+            <?php the_post_thumbnail('medium_large'); ?>
+        </a>
         <?php endif; ?>
-
-        <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-
-        <?php if ('post' === get_post_type()): ?>
-        <div class="entry-meta">
-            <?php inhabitent_posted_on(); ?> /
-            <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> /
-            <?php inhabitent_posted_by(); ?>
-        </div><!-- .entry-meta -->
-        <?php endif; ?>
-    </header><!-- .entry-header -->
-
-    <div class="entry-content">
-        <?php the_excerpt(); ?>
-    </div><!-- .entry-content -->
-</article><!-- #post-## --> 
+    </div>
+    <div class="product-info">
+        <h2 class="product-title">
+            <?php the_title(); ?>
+        </h2>
+        <span class="price">
+            <?php $product_terms = get_terms('product_type'); ?>
+            <?php echo $product_terms->price; ?>
+        </span>
+    </div>
+</div> 
